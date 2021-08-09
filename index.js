97953bb19846da47dc6cd8f95d9493c9bffc079a
mvcbohyun <jbh959847@gmail.com>
2021-08-09 15:12:23 +0900

    그랩마켓 서버 시작

---
+++ index.js
@@ -0,0 +1,23 @@
+//index.js 
+var http = require('http'); // node 내장 모듈 불러옴 
+var hostname = '127.0.0.1'; // localhost와 동일 
+var port = 8085; 
+
+http.createServer(function(req, res){
+    const url = req.url; //req.url에는 port와 ip번호를 제외한 url이 들어가게 됩니다.
+    const method = req.method;
+    if(url ==='/products'){ //아티클 정보를 받아오는 요청
+        if(req.method === 'GET'){ //멤버들 정보를 받아오는 요청
+            res.writeHead(200,{'Content-Type':'application/json'})
+            const products = JSON.stringify({
+                name:"농구공",
+                price:5000
+            });
+            res.end(products);
+        }else if(req.method === 'POST'){ //멤버가 회원가입을 할 때 요청
+            res.end("생성되었습니다.");
+        }
+}
+}).listen(port, hostname); 
+
+console.log('Server running at http://'+hostname+':'+port);
